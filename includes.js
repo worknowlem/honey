f5c2c91bd853f6aea6ae9bd41cf2830a2c154c04
worknowlem <worknowlem@gmail.com>
2025-08-15 10:33:35 +0800

    root files
    important! index.html, includes script, styles, header, footer, about and error page uploaded.

---
+++ includes.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("/header.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    });
+
+  fetch("/footer.html")
+    .then(res => res.text())
+    .then(data => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
